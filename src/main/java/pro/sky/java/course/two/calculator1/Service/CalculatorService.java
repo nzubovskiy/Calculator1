cc43c2231da275a912def7e338fb002153ee771f
NIKO <nzubovskiy@gmail.com>
2022-05-29 12:42:18 +0400

    Homework

---
+++ src/main/java/pro/sky/java/course/two/calculator1/Service/CalculatorService.java
@@ -0,0 +1,9 @@
+package pro.sky.java.course.two.calculator1.Service;
+
+public interface CalculatorService {
+    String showGreetings();
+    String plus(float num1, float num2);
+    String minus(float num1, float num2);
+    String multiply(float num1, float num2);
+    String divide(float num1, float num2);
+}
